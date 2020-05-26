75b868b6ab951e1d2a04b4d80006aa351a78302a
Jerry Hsu <cba5530@gmail.com>
2020-05-27 00:17:29 +0800

    2nd version

--- guess-number.py
+++ guess-number.py
@@ -1,10 +1,13 @@
 import random
 r = random.randint(1,100)
+x = 0
 while True:
+	x += 1
 	num = input('Please guess a number:')
 	num = int(num)
 	if num == r:
 		print('Your right!')
+		print('Totally guess',x,'times')
 		break
 	elif num < r:
 		print('I want more!')
